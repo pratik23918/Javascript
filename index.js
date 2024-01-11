d7572736ab06ed07d20a4269cbdd1d1f127e48e6
PRATIK KUMAR SINGH <rishusingh3573@gmail.com>
2024-01-11 16:19:55 +0530

    commit
    function in js

--- index.js
+++ index.js
@@ -4,3 +4,20 @@ function getMethod(){
     console.log("I'm fine");
 }
 getMethod();
+// parametrized function
+function calculate(amtOfBottles){
+    var price=amtOfBottles*5;
+    console.log("you have bought"+ price);
+}
+calculate(3);
+//ex: code toLife in Weeks Coding Exercise , the life exist till 90 years age
+function lifeInWeeks(age) {
+    //Write your code here.
+    var result=90-age;
+    var daysleft=result*365;
+    var weeksleft=result*52;
+    var monthsleft=result*12;
+    console.log("You have "+ daysleft+" days, "+weeksleft+" weeks,"+" and "+monthsleft+" "+"months left.");
+
+}
+lifeInWeeks(12);
